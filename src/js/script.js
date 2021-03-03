1c12ff29ac4ae1669b102efb4ff0e326e98765be
Paweł Waśkiewicz <pawel.waskiewicz@gmail.com>
2021-03-03 13:48:22 +0100

    add determineRatingBgc

--- src/js/script.js
+++ src/js/script.js
@@ -22,6 +22,9 @@
   
   function render(){
     for(let book of dataSource.books){
+
+      book.ratingBgc = determineRatingBgc(book.rating);
+      book.ratingWidth = determineRatingwidth(book.rating);
       // generate html basen on template
       const generateHTML = templates.templateBook(book);
       console.log(book);
@@ -33,7 +36,6 @@
       // add DOM element to books-list
       booksList.appendChild(generateDOM);
 
-     
     }
   }
   
@@ -91,28 +93,85 @@
   function filterBooks(){
     
 
-    for(const book in dataSource.books){
-      console.log(allFilters);
+    for(const element of dataSource.books){
       let shouldBeHidden = false;
 
       for(const filter of allFilters) {
-        if(!book.details[filter]) {
+        if(!element.details[filter]) {
           shouldBeHidden = true;
           break;
         }
       }
       if(shouldBeHidden){
-        const book = document.querySelector('book__image[data-id="' + book.id + '"]');
+        const book = document.querySelector('.book__image[data-id="' + element.id + '"]');
         book.classList.add('hidden');
       } else {
+        const book = document.querySelector('.book__image[data-id="' + element.id + '"]');
         book.classList.remove('hidden');
       }
       
     }
   }
+
+  function determineRatingBgc(rating){
+    
+    let background = '';
+
+    if(rating < 6) { 
+      background = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
+    } else if (rating > 6 && rating <= 8) { 
+      background = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+    } else if (rating > 8 && rating <=9) { 
+      background = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+    } else if(rating > 9) { 
+      background = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+    }
+
+    return background;
+  }
+
+  function determineRatingwidth(rating){
+
+    let Width = 0;
+
+    if (rating > 1) {
+      Width = 10;
+    }
+    if (rating > 2) {
+      Width = 20;
+    }
+    if (rating > 3) {
+      Width = 30;
+    }
+    if (rating > 4) {
+      Width = 40;
+    }
+    if (rating > 5) {
+      Width = 50;
+    }
+    if (rating > 6) {
+      Width = 60;
+    }
+    if (rating > 7) {
+      Width = 70;
+    }
+    if (rating > 8) {
+      Width = 80;
+    }
+    if (rating > 9) {
+      Width = 90;
+    }
+    if (rating > 10) {
+      Width = 100;
+    }
+
+    return Width;
+  }
   
 
   render();
+  determineRatingBgc();
+  determineRatingwidth();
   initActions();
 
  
